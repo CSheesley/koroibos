3f662ade2d89ef28ba54c204ab6a41c054e533ad
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-27 18:34:07 -0600

    Adds temporary placeholder_spec.rb. Adds PR Template

---
+++ spec/placeholder_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Placeholder Test' do
+  context 'it is used for testing RSpec and CI set-up' do
+    it 'passes' do
+
+      expect(1 + 2).to eq(3)
+    end
+  end
+end
