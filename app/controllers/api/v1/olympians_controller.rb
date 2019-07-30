da042dd55dbaddfc5fbae15c7f5480c6b0de3c9d
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-29 19:00:18 -0600

    Adds api/v1/olympians?age=<> Endpoint and Specs. (youngest, oldest, age=n)

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,29 @@
 class Api::V1::OlympiansController < ApplicationController
 
   def index
-    render json: Olympian.all
+    if youngest?
+      render json: Olympian.youngest
+    elsif oldest?
+      render json: Olympian.oldest
+    elsif find_by_age?
+      render json: Olympian.where(age: params[:age])
+    else
+      render json: Olympian.all
+    end
   end
+
+private
+
+    def youngest?
+      params[:age] == "youngest"
+    end
+
+    def oldest?
+      params[:age] == "oldest"
+    end
+
+    def find_by_age?
+      params[:age].to_i > 0
+    end
 
 end
